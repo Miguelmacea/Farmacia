9b4c615206bf1e7dbe52c599ad3d115f3a854f65
neon <neon@ingeniero-aspirea51543>
2023-11-16 16:17:22 -0500

    7

---
+++ Farmacia/src/farmacia/LoginController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package farmacia;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author neon
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField t1;
+    @FXML
+    private TextField t2;
+    @FXML
+    private Button log;
+    @FXML
+    private Button sal;
+    @FXML
+    private Button regg;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void logear(ActionEvent event) {
+    }
+
+    @FXML
+    private void salida(ActionEvent event) {
+    }
+
+    @FXML
+    private void registrase(ActionEvent event) {
+    }
+    
+}
